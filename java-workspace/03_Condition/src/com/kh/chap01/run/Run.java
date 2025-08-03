d18aaaf5e588deeb526c4a5c6613c8fa3286c378
suelina001 <elinaen001z@gmail.com>
2025-08-03 21:47:44 +0900

    A_if_assginment

--- java-workspace/03_Condition/src/com/kh/chap01/run/Run.java
+++ java-workspace/03_Condition/src/com/kh/chap01/run/Run.java
@@ -1,7 +1,7 @@
 package com.kh.chap01.run;
 
 import com.kh.chap01.condition.A_If;
-
+import com.kh.chap01.condition.A_If_Asignmt;
 public class Run {
 	
 	public static void main(String[] args) {
@@ -12,7 +12,12 @@ public class Run {
 		//a.lunchMenu();
 		
 		//a.method3();
-		a.method4();
+		//a.method4();
+		
+		
+		
+		A_If_Asignmt t = new A_If_Asignmt();
+		t.menuOrder();
 	}
 	
 }
