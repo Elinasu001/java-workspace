586bbc60dda01f3f51617d1ff682c66358c70fc0
suelina001 <elinen001@gmail.com>
2025-08-08 20:27:10 +0900

    re

--- 05_Object/src/com/kh/example/pratice1/run/Run.java
+++ 05_Object/src/com/kh/example/pratice1/run/Run.java
@@ -5,7 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		
 		Member member = new Member();
-		member.changeName("홍길동");
+		member.changeName("박수현 ");
 		member.printName();
 	}
 
