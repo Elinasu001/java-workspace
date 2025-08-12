528e965fdfe14eaaf09f18890360e802ced0d9ce
suelina001 <elinen001@gmail.com>
2025-08-12 12:39:55 +0900

    abstract

---
+++ 08_Polymorphism/src/com/kh/chap03/model/vo/GateBall.java
@@ -0,0 +1,15 @@
+package com.kh.chap03.model.vo;
+
+
+//방법 2 : 나를 추상화하여 나를 상속 받는 누가 구체화 시켜~
+public abstract class GateBall extends Sports {
+	
+	// abstract 된 Sports 를 extends 하려면?
+	// 방법 1 : 추상 메소드를 구현해준다.
+	/*
+	@Override
+	public void rule() {
+		
+	}
+	*/
+}
--- 08_Polymorphism/src/com/kh/chap03/run/Run.java
+++ 08_Polymorphism/src/com/kh/chap03/run/Run.java
@@ -17,8 +17,17 @@ public class Run {
 		// 이게 문제임 : 
 		// 순전히 자식들에게 상속을 해줄 용도로 만든 클래스인데
 		// 이것을 가지고 객체를 생성 하게 되면 문제가 될 수 있음.
-		//Sports s = new Sports(); : 추상메소드 abstract 로 인해서 빨간줄 생김.
+		// 추상클래스로 선언되었기 때문에 객체 생성이 불가능함
+		//Sports s = new Sports(); 
 		//s.rule();
+		// 다형성은 적용가능 !!
+		Sports[] sports = new Sports[2];
+		sports[0] = fb;
+		sports[1] = bb;
+		
+		for(int i = 0; i < sports.length; i ++) {
+			sports[i].rule();
+		}
 	}
 
 }
