25e7e131d6d5b933701856e50e4489dddac8a68c
suelina001 <elinen001@gmail.com>
2025-08-18 17:51:13 +0900

    view 에서 식무ㅡㄹ 삭제

--- 13_MVC_project/src/com/kh/plant/controller/PlantController.java
+++ 13_MVC_project/src/com/kh/plant/controller/PlantController.java
@@ -18,7 +18,7 @@ public class PlantController {
 		plants.add(new Plant("안시리움" , "분재"));
 	}
 	
-	// View 에서 식물 추가 요청을 할 때마다 호출되는 메소드
+	// 1. View 에서 식물 추가 요청을 할 때마다 호출되는 메소드
 	public void createPlant(String name, String type) {
 		// 1절 => 객체 생성
 		Plant plant = new Plant(name, type);
@@ -30,10 +30,57 @@ public class PlantController {
 		//true false/ 받아 주면 좋긴한데 없으니 ... 
 	}
 	
-	// View 에서 전체 식물조회 요청이 왔을 때 호출되는 메소드 (getter)
+	// 2. View 에서 전체 식물조회 요청이 왔을 때 호출되는 메소드 (getter)
 	public List<Plant> readAll() {
-		// Plant 타입의 객체들의 주소가 요소로 존재하는 Objectp[] View에게 반환
+		// Plant 타입의 객체들의 주소가 요소로 존재하는 Object[] View 에게 반환
 		return plants;
+	}
+	
+	// 4. View 에서 식물 삭제 요청이 왔을 때 호출되는 메소드
+	public int detetePlant(String name, String type) {
+		/*
+		 * 사용자가 입력한 식물 이름 + 식물 종류와 일치하는 필드값을 가진 Plant 객체를 제거
+		 * 
+		 * case 1. 동일한 이름 및 종류를 가진 식물이 존재하지 않았다
+		 * case 2. 동일한 이름 및 종류를 가진 식물이 딱 한 개 존재했다.
+		 * case 3. 동일한 이름 및 종류를 가진 식물이 여러 개 존재했다.
+		 * 
+		 * 성공 / 실패에 따라서 View 에게 반환 값을 다르게 돌려 주어야 함.
+		 */
+		
+		/*
+		 * 내 생각으로 써본 코드 ...
+		for(Plant p : plants) {
+			if(p.getName().equals(name) && p.getType().equals(type)) {
+				return 1;
+			}
+		}
+		return 0;
+		*/
+		
+		//Plant p = plants.remove(2);
+		//List returnList = new ArrayList();
+		//returnList.add(p);
+		//return returnList;
+		
+		/*
+		 * 요소가 삭제 될 때마다 정수값을 증가 시키고
+		 * 증가된 정수만 반환
+		 */
+		int count = 0;// 지워진 개수 만큼 돌아감
+			
+		// 요청 처리 
+		for(int i = 0; i < plants.size(); i++) {
+			// 방어적으로 작성하기
+			if(name.equals(plants.get(i).getName()) &&
+				type.equals(plants.get(i).getType())) {
+				plants.remove(plants.get(i)); // 요소를 제거
+				i--; // 2절
+				count++; // 지워질 때마다 ++
+			}
+		}
+		
+		return count;
 	}
 	
 }
--- 13_MVC_project/src/com/kh/plant/view/PlantView.java
+++ 13_MVC_project/src/com/kh/plant/view/PlantView.java
@@ -46,8 +46,8 @@ public class PlantView {
 				switch(menuNo) {
 				case 1: createPlant(); break;
 				case 2: readAll(); break;
-				case 3:break;
-				case 4:break;
+				case 3: findByKeyWord(); break;
+				case 4: deletePlant(); break;
 				case 5:break;
 				case 6:break;
 				case 0: System.out.println("프로그램을 종료합니다."); break;
@@ -86,8 +86,9 @@ public class PlantView {
 	
 	// 2. 전체 식물 목록을 출력해주는 기능
 	private void readAll() {
-		
-		System.out.println("\n 전체 식물 목록입니다.");
+		System.out.println();
+		System.out.println("===================================================");
+		System.out.println("전체 식물 목록입니다.");
 		
 		List<Plant> plants = pc.readAll();
 		
@@ -95,16 +96,74 @@ public class PlantView {
 		// case 1. 값이 없을 경우 
 		// case 2. 값이 하나의 경우 
 		// case 2. 값이 여러개일 경우 
-		
-		// List의 요소가 하나도 존재하니 않을 경우
+		// List 의 요소가 하나도 존재하니 않을 경우
 		if(plants.isEmpty()) { // (plants.size() == 0) 이것보다 isEmpty() 을 더 선호
 			System.out.println("식물이 존재하지 않습니다.");
 			System.out.println("새로운 식물을 추가해보세요.");
 		} else {
 			// 최소한 하나라도 있을 경우
 			for(Plant p : plants) {
+				System.out.println("---------------------------------------------------");
 				System.out.println("식물이름 : " + p.getName() + ", 식물종 : " + p.getType());
 			}
 		}
+	}
+	
+	// 3. 사용자가 식물에 대한 키워드를 입력할 수 있는 화면을 출력해주는 기능
+	private void findByKeyWord() {
+		/*
+		 * 1. 사용자가 입력한 키워드가 제목에 포함되어있다면 출력
+		 * 2. 사용자가 입력한 키워드가 타입에 포함되어있다면 출력
+		 * 3. 사용자가 입력한 키워드가 제목 또는 타입에 포함되어있다면 출력 [v]
+		 * 4. 사용자가 입력한 키워드가 제목과 타입에 둘 다 포함되어있다면 출력
+		 * 
+		 */
+		
+		// 3)
+		System.out.println("식물 검색 서비스입니다.");
+		System.out.println("검색어를 입력해주세요 > ");
+		String keyword = sc.nextLine();
+		// View 가 할 일 끝 !
+		
+	}
+	
+	// 4. 특성 식물을 삭제할 수 있는 화면을 출력해주는 기능
+	private void deletePlant() {
+		System.out.println();
+		// 삭제를 어떻게 할 수 있도록 만들어 줄 것인가??
+		
+		/*
+		 * 1. 식물 이름을 입력받아서 삭제
+		 * 2. 식물 타입을 입력받아서 삭제
+		 * 3. 식물이름과 타입을 입력받아서 삭제 [v]
+		 * 		1) 이름만 일치하면 삭제
+		 * 		2) 타입만 일치하면 삭제
+		 * 		3) 이름하고 타입하고 둘 다 일치해야 삭제 [v]
+		 * 		4) 이름 키워드가 포함되어 있으면 삭제
+		 * 		5) 타입 키워드가 포함되어 있으면 삭제
+		 * 		6) 이름 또는 타입에 키워드가 포함되어 있으면 삭제
+		 * 4. 그냥 아무것도 입력안받고 전체 삭제
+		 * 
+		 */
+		
+		// 3-3) 이름하고 타입을 입력받고 둘 다 일치하는 값이 있을 때만 삭제 [v]
+		System.out.println("식물의 이름을 입력해주세 > ");
+		String name = sc.nextLine();
+		System.out.println("식물의 종류를 입력해주세요 > ");
+		String type = sc.nextLine();
+		// 일단 할 일 끝 !!
+		
+		// 컨트롤러에게 사용자가 입력한 값 두 개를 전달하면서 삭제요청 보내기
+		int count = pc.detetePlant(name, type); // 0일지 1일지 모르니 변수에 담아주기
+		
+		// 0
+		// 1 ↑
+		if(count > 0) {
+				System.out.println("삭제 성공~!");
+				System.out.println(name + "을(를) 총 " + count + "개 삭제했습니다.!");
+		} else {
+			System.out.println("삭제할 식물을 찾지 못했습니다.");
+		}
+		System.out.println();
 	}
 }
